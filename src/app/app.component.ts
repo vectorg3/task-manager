2c81de4fd6b499777c21a658b7ba7996f334b18b
vectorg3 <diglytail@gmail.com>
2023-07-05 12:47:32 +0300

    app theme switcher implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'task-manager';
+  darkMode = false;
+  modeToggle() {
+    this.darkMode = !this.darkMode;
+    document.documentElement.setAttribute(
+      'data-theme',
+      this.darkMode ? 'dark' : 'light'
+    );
+  }
 }
